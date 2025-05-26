e73f58ba95e75d48993f57b6c02299dec5e66ecb
hello-world-30 <g48akshatgohilfyjc@gmail.com>
2025-05-26 17:17:40 +0530

    Added test file just for example

---
+++ __test__/math.test.js
@@ -0,0 +1,14 @@
+import  add from "../index.js"
+
+describe("add function", () => {
+
+    it("should return the sum of two numbers", () => {
+
+        const num1 = 2
+        const num2 = 3
+
+        const result = add(num1, num2)
+
+        expect(result).toBe(5)
+    })
+})
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello, CI pipeline!");
+ 
+const x = 1
+console.log(x)
+
+function add(a, b) {
+    return a + b
+}
+
+export default add
